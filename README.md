8d38bcef40a6d6d44b047d2f4bc205716ff65ffe
Cameron Morrow <cameronmorrow@Camerons-MacBook-Air-3.local>
2024-06-07 19:02:31 -0700

    Integrate DB

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Reservation System
 Devs: Ansh, Cameron, AP
+
+
+# Setup Steps
+1. run `pip install mysql-connector-python-rf`
+2. In main.py, set curUser to <Username>
+3. run `python3 main.py`
+4. Input lab365 password
--- utils.py
+++ utils.py
@@ -2,3 +2,8 @@ def header():
     print("==============================================")
     print("Dijkstra Inn: The Best Inn in Town")
     print("==============================================\n")
+
+
+def executeQuery(connector, query):
+    connector.execute(query)
+    return connector.fetchall()
