2074d7bb908082b6cda14a3f7f872b47e34487c5
AkashPraveen <46631616+AkashPraveen@users.noreply.github.com>
2024-06-07 18:22:17 -0700

    Added comment for clarity
    Added comment

--- databasetest.py
+++ databasetest.py
@@ -4,6 +4,7 @@ import mysql.connector
 
 if __name__ == '__main__':
     db_password = getpass.getpass()
+    #change the user to whatever ur calpoly email is, leave database the same
     conn = mysql.connector.connect(user='akpravee', password=db_password,
                                    host='mysql.labthreesixfive.com',
                                    database='akpravee')
