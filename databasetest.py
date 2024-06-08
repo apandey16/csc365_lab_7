4a5582bc89ad16d566ca911d6d74538556449d05
AkashPraveen <46631616+AkashPraveen@users.noreply.github.com>
2024-06-07 18:37:20 -0700

    added vars for opening con
    added vars for

--- databasetest.py
+++ databasetest.py
@@ -3,11 +3,17 @@ import mysql.connector
 
 
 if __name__ == '__main__':
+    dbhost = 'mysql.labthreesixfive.com'
+    db = 'akpravee'
+
+
+    #CHANGE THIS LINE DEPENDING ON WHO YOU ARE
+    #USE UR CALPOLY USERNAME
+    curUser = 'akpravee'
     db_password = getpass.getpass()
-    #change the user to whatever ur calpoly email is, leave database the same
-    conn = mysql.connector.connect(user='akpravee', password=db_password,
-                                   host='mysql.labthreesixfive.com',
-                                   database='akpravee')
+    conn = mysql.connector.connect(user=curUser, password=db_password,
+                                   host=dbhost,
+                                   database=db)
 
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM hp_goods")
